af91080439565add4f06327ecfe0ba1c6466e3aa
zan <zan_zibert@hotmail.com>
2018-11-02 16:14:07 +0100

    #7 - Project Details Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import NavBar from './components/layout/NavBar'
 import DashBoard from './components/dashboard/Dashboard';
+import ProjectDetails from './components/projects/ProjectDetails'
 
 
 class App extends Component {
@@ -11,7 +12,8 @@ class App extends Component {
         <div className="App">
           <NavBar />
           <Switch>
-            <Route path='/' component={DashBoard} />
+            <Route exact path='/' component={DashBoard} />
+            <Route path='/project/:id' component={ProjectDetails}/>
           </Switch>
         </div>
       </BrowserRouter>
