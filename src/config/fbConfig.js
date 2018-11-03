6b4c4f7966a50dc7fae8169ce208a08ea348f04f
zan <zan_zibert@hotmail.com>
2018-11-03 13:42:48 +0100

    #15 - Creating a Firebase Project

---
+++ src/config/fbConfig.js
@@ -0,0 +1,17 @@
+import Firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDpzvGvuDlFcYaqJjzlPgJgCuDJFM3p4a8",
+    authDomain: "zzibert-mario-plan.firebaseapp.com",
+    databaseURL: "https://zzibert-mario-plan.firebaseio.com",
+    projectId: "zzibert-mario-plan",
+    storageBucket: "zzibert-mario-plan.appspot.com",
+    messagingSenderId: "177060236240"
+  };
+  firebase.initializeApp(config);
+  firebase.firestore().settings({ timestampsInSnapshots: true })
+
+  export default firebase
