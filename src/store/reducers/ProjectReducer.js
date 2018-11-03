61220bc2c5b9e2d4c00e4f9dacbbcd86f5327903
zan <zan_zibert@hotmail.com>
2018-11-03 12:01:11 +0100

    #12 - Adding Dummy Data

--- src/store/reducers/ProjectReducer.js
+++ src/store/reducers/ProjectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+    projects: [
+        {id: 1, title: 'help me find peach', content: 'bla bla bla'},
+        {id: 2, title: 'collect all the start', content: 'bla bla bla'},
+        {id: 3, title: 'egg hunt with yoshi', content: 'bla bla bla'},
+    ]
+}
 
 const ProjectReducer = (state = initState, action) => {
     return state
