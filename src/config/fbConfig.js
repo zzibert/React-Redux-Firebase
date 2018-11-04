9a329bac10f3d452d15b2cd0446300ea6edbda1f
zan <zan_zibert@hotmail.com>
2018-11-04 12:28:19 +0100

    #18 - Adding Data to Firestore

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -1,4 +1,4 @@
-import Firebase from 'firebase/app'
+import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
 
--- src/index.js
+++ src/index.js
@@ -13,9 +13,10 @@ import fbConfig from './config/fbConfig'
 
 const store = createStore(RootReducer, 
     compose(
-        applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore}))),
+        applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
         reduxFirestore(fbConfig),
         reactReduxFirebase(fbConfig)
+    )
 )
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
